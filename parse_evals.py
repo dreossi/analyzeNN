68bb00ba66b395f91ecb95a766456f5505ae9908
tommasodreossi <tommasodreossi@gmail.com>
2017-12-08 12:19:18 -0800

    eval tools added

---
+++ parse_evals.py
@@ -0,0 +1,37 @@
+'''Extarct infos from evaluation files'''
+
+eval_dir="/home/tommaso/analyzeNN/data/train_0/eval/test_2"
+eval_file="/home/tommaso/analyzeNN/data/train_0/eval/test_1/200.txt"
+
+import matplotlib.pyplot as plt
+
+
+def get_data(file_name):
+    '''Get data from evaluation file'''
+    f = open(file_name,"r")
+    eval_cont = f.read()
+    easy_acc_i = eval_cont.find("car_easy: ")
+    medium_acc_i = eval_cont.find("car_medium: ")
+    hard_acc_i = eval_cont.find("car_hard: ")
+    easy_acc = float(eval_cont[easy_acc_i+10:easy_acc_i+15])
+    medium_acc = float(eval_cont[medium_acc_i+12:medium_acc_i+17])
+    hard_acc = float(eval_cont[hard_acc_i+10:hard_acc_i+15])
+    return easy_acc, medium_acc, hard_acc
+
+def get_data_dir(dir_path, checkpoints):
+    '''Get data from evaluation directory'''
+    easys = []
+    mediums = []
+    hards = []
+    for checkpoint in checkpoints:
+        file_name = str(checkpoint) + '.txt'
+        easy, medium, hard = get_data(dir_path + '/' + file_name)
+        easys += [easy]
+        mediums += [medium]
+        hards += [hard]
+    return easys, mediums, hards
+
+checkpoints = range(0,3000,200)
+easys, mediums, hards = get_data_dir(eval_dir, checkpoints)
+plt.plot(checkpoints, easys, 'r-', checkpoints, mediums, 'g-', checkpoints, hards, 'b-',)
+plt.show()
